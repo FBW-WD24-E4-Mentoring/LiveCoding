3552d667575bcf288ee3ac8c6b5d6bfab2be923a
nrcool <nomanalam7214@gmail.com>
2024-08-30 12:17:02 +0200

    recap array, object

--- recap-javascript/main.js
+++ recap-javascript/main.js
@@ -1,10 +1,25 @@
 /* String */
 
-const myName = "Joseph";
+let myName = "Josephos";
+/* create */
+/* const updatedName = myName.concat(" Smith", ",Developer!"); */
+/* const updatedName = myName + " Smith" + ",Developer!"; */
 
-myName.concat(" Smith");
+/* const updatedName = `${myName} Smith,Developer!`; */
 
-console.log(myName);
+/* console.log(updatedName); */
+
+/* read */
+/* console.log(myName[0]);
+console.log(myName[myName.length - 1]); */
+
+/* update */
+/* const updated = myName.replace(/Jo/gi, "Te");
+console.log(updated); */
+
+/* delete */
+/* console.log(myName.replace("os", "")); */
+/* console.log(myName.replace(/os/g, "")); */
 /* console.log(myName.length);
 
 console.log(myName.slice(0, 3));
@@ -23,7 +38,12 @@ console.log(myName.indexOf("s")); */
 console.log(nullArray); */
 
 /* performing crud operation on array */
-const cities = ["Berlin", "Frankfurt", "Hamburg", "Leibzig"];
+const cities = [
+  "Berlin",
+  "Frankfurt",
+  "Hamburg",
+  "Leibzig",
+]; /* add multiple items */ /* add 1 item */
 
 /* Creation */
 
@@ -34,8 +54,8 @@ const cities = ["Berlin", "Frankfurt", "Hamburg", "Leibzig"];
 /* cities.unshift("Bonn"); */
 
 /* insert item on specific index/position */
-/* cities.splice(2, 0, "Bonn", "Dusseldorf"); */ /* add multiple items */
-/* cities.splice(2, 0, "Bonn"); */ /* add 1 item */
+/* cities.splice(2, 0, "Bonn", "Dusseldorf"); */
+/* cities.splice(2, 0, "Bonn"); */
 
 /* Read */
 
@@ -56,10 +76,10 @@ console.log(cities[2]); */
 } */
 
 /* using forEach method */
-cities.forEach((item, index) => {
+/* cities.forEach((item, index) => {
   console.log(item);
   console.log(index);
-});
+}); */
 
 /* cities.forEach(console.log); */
 
@@ -93,3 +113,161 @@ console.log(cities); */
 /* console.log(Array.isArray(cities));
 
 cities.reverse(); */
+
+/* Object */
+
+/* literal
+ObjectConstructor
+functional
+constructor/class */
+
+/*  literal */
+const student = {
+  name: "Leon",
+  age: 23,
+  printDetail() {
+    console.log(this.name);
+  },
+};
+console.log(student);
+/* ObjectConstructor */
+const student1 = new Object();
+student1.name = "leon";
+student1.age = 23;
+student1.printDetail = function () {
+  console.log(this.name);
+};
+console.log(student1);
+
+/* functional */
+function createObject(name, age) {
+  return {
+    name,
+    age,
+    printDetail() {
+      console.log(this.name);
+    },
+  };
+}
+
+const student2 = createObject("Robin", 27);
+console.log(student2);
+
+/* Custom Constructor / factory function */
+/* way before es6 --ecma2015 */
+function MyCustomConstrcutor(name, age) {
+  /*  create {} called this */
+  this.name = name;
+  this.age = age;
+  /* return this */
+}
+/* method in my constructor */
+MyCustomConstrcutor.prototype.printDetail = function () {
+  console.log(this.name);
+};
+
+const student3 = new MyCustomConstrcutor(
+  "Max",
+  24
+); /* => create {} called this */
+
+/* es6 class format to create constructor */
+
+/* class DCI {
+  #privateProperty = "secretcode";
+  constructor(name, duration) {
+    this.name = name;
+    this.duration = duration;
+  }
+  #privateMethod() {
+    return this.#privateProperty;
+  }
+  showSecretKeyWithChanges() {
+    return this.#privateMethod() + "123Abc34";
+  }
+  printDetail() {
+    console.log(this.name);
+  }
+  static compareBoth(a, b) {
+    if (a instanceof this === b instanceof this) {
+      console.log("both instances are from the same class");
+    } else {
+      console.log("they belongs to different classes");
+    }
+  }
+} */
+
+/* const webDevCourse = new DCI("web dev", 12);
+
+const marketingCourse = new DCI("marketing", 6); */
+/* instances of DCI */
+
+/* DCI.compareBoth(webDevCourse, marketingCourse); */
+
+class DCIEmployee {
+  #salary = null;
+  constructor(name, course, salary) {
+    this.name = name;
+    this.course = course;
+    this.#salary = salary;
+  }
+  static showSalary(instance) {
+    console.log(instance.#salary);
+  }
+}
+
+const employee1 = new DCIEmployee("Max", "web dev", 3000);
+const employee2 = new DCIEmployee("John", "marketing", 4000);
+
+DCIEmployee.showSalary(employee2);
+
+let user = {
+  name: "Rbin",
+  age: 20,
+};
+
+/* reading */
+/* console.log(user.name);
+console.log(user.age); */
+
+/* reading => destructuring */
+/* const { name, age } = user;
+
+
+/* reading all keys */
+/* const keys = Object.keys(user);
+console.log(keys);
+const values = Object.values(user);
+console.log(values);
+const entries = Object.entries(user);
+console.log(entries); */
+
+for (const key in user) {
+  console.log(key);
+  console.log(user[key]); /* get value of that key */
+}
+
+console.log("age" in user);
+console.log("address" in user);
+
+/* for values */
+console.log(Object.values(user).includes("Rbin"));
+
+console.log(user.hasOwnProperty("name"));
+
+/* console.log(name);
+console.log(age);
+ */
+/* update */
+/* const user = {
+  name: "Robin",
+  age: 20,
+}; */
+
+/* user.name = "Nikolas"
+user.age=32; */
+
+/* delete */
+/* delete user.age;
+
+console.log(user); */
